465d756120263e5b8810bc017aa1a867d8fa046e
h0rhay <clarkgeorge76@gmail.com>
2018-10-22 17:34:29 +0100

    sort out reversing of accordion logic for sidenav to ensure animations run

--- src/components/SideBarListItem.js
+++ src/components/SideBarListItem.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import colors from '../assets/styles/colors'
+import styled from 'styled-components'
 
+const LiComponent = styled.li`
+  a {
+    color: ${colors.$white};
+    &:hover {
+      color: ${colors.$primary};
+    }
+    h6 {
+      margin-left:0.5rem;
+    }
+  }
+`
 
-const ListItem =  ({post}) => {
+const SideBarListItem = ({post}) => {
   return (
-  <li className='mob-text-center' key={post.node.id} style={{listStyleType: 'none'}}>
+  <LiComponent className='mob-text-center' key={post.node.id} style={{listStyleType: 'none'}}>
     <Link to={post.node.frontmatter.path} key={post.node.id}>
       <h6>{post.node.frontmatter.doc_title}</h6>
     </Link>
-  </li>
+  </LiComponent>
   )
 }
 
-export default ListItem
+export default SideBarListItem
--- src/components/SideBarNavCollapse.js
+++ src/components/SideBarNavCollapse.js
@@ -1,79 +1,66 @@
 import React from 'react'
-import SideBarListItem from './SideBarListItem'
-
-import {language} from '../assets/utils/language'
+import NavLinksCollapse from './NavLinksCollapse'
 import colors from '../assets/styles/colors'
 import styled from 'styled-components'
 import FaChevronRight from '../assets/images/chevron.svg'
+
+const NavCollapse = styled.div`
+    .nav-collapse {
+      transition: max-height 0.3s ease-out;
+      overflow: hidden;
+    }
+    .nav-collapse.closed {
+      max-height:0 !important;
+    }
+    h4 {
+      display: inline-block;
+      cursor: pointer;
+      margin: 0 0 0.5rem 0;
+      position: relative;
+      &:after {
+        transition: all 0.25s ease-out;
+        content: '';
+        background: url(${FaChevronRight}) no-repeat 100% 25%;
+        position: absolute;
+        top: 0.2rem;
+        right: -15px;
+        height: 12px;
+        width: 12px;
+      }
+    }
+    h4.open {
+      &:after {
+        transform: rotate(90deg);
+      }
+    }
+  `
 
 class SideBarNavCollapse extends React.Component {
   constructor (props) {
     super (props)
     this.state = {
-      toggle: props.active ? props.active : false
-    }
+      toggle: props.active ? props.active : false,
+      linksToggle: false
+    } // TODO: Add prevState checker to this set State call
     this.handleCheck = this.handleCheck.bind(this)
   }
 
   handleCheck () {
-    this.setState(prevState => ({toggle: !prevState.toggle}))
-  }
-
-  getNavCollapseComponent (height) {
-    return styled.div`
-      .nav-collapse {
-        transition: max-height 0.3s ease-out;
-        overflow: hidden;
-        max-height:0;
-      }
-      .nav-collapse.open {
-        max-height: ${height}px;
-      }
-      h4 {
-        display: inline-block;
-        cursor: pointer;
-        padding: 0 0 0 0;
-        margin: 0;
-        position: relative;
-        &:after {
-          transition: all 0.25s ease-out;
-          content: '';
-          background: url(${FaChevronRight}) no-repeat 100% 25%;
-          position: absolute;
-          top: 0.2rem;
-          right: -15px;
-          height: 15px;
-          width: 15px;
-        }
-      }
-      h4.open {
-        &:after {
-          transform: rotate(90deg);
-        }
-      }
-
-    `
+    this.setState(prevState => ({
+      toggle: !prevState.toggle,
+      linksToggle: !prevState.linksToggle
+    }))
   }
 
   render () {
     const props = this.props
-    const postList = props.postList
-    const NavCollapse = this.getNavCollapseComponent(this.myNav ? this.myNav.offsetHeight : 0)
     return (
       <div>
         <NavCollapse className="collapse-nav-component">
-          <h4 style={{color: colors.$primary}} onClick={this.handleCheck} className={this.state.toggle ? 'open' : 'closed'}>
+          <h4 style={{color: colors.$primary}} onClick={this.handleCheck} className={(this.state.toggle || this.state.linksToggle) ? 'open' : 'closed'}>
             {props.section}
           </h4>
-          <div className={`nav-collapse ${this.state.toggle ? 'open' : 'closed'}`}>
-            <ul className='list-group list-unstyled' id={props.id} ref={(myNav) => this.myNav = myNav} >
-              {postList.edges.map( (post, i) => (
-                (post.node.frontmatter.language === language && post.node.frontmatter.label === 'docs' && post.node.frontmatter.group === props.group) &&
-                  <SideBarListItem post={post} key={i}/>
-                )
-              )}
-            </ul>
-          </div>
+          <NavLinksCollapse toggle={this.state.toggle} linksToggle={this.state.linksToggle} postList={props.postList} group={props.group}/> 
         </NavCollapse>
       </div>
     )
